f5a2a5069b780fd012677faf85c41b35e180adef
claudxfiles <claudio.alcaman@gmail.com>
2025-04-01 22:08:48 -0300

    feat: nueva funcionalidad

--- frontend/src/types/supabase.ts
+++ frontend/src/types/supabase.ts
@@ -1,16 +1,3 @@
-import { Database } from './database.types';
-
-export type Tables = Database['public']['Tables'];
-export type TablesInsert = {
-  [TableName in keyof Tables]: Tables[TableName]['Insert']
-};
-export type TablesUpdate = {
-  [TableName in keyof Tables]: Tables[TableName]['Update']
-};
-export type TablesRow = {
-  [TableName in keyof Tables]: Tables[TableName]['Row']
-};
-
 // Tipado para las funciones de Supabase
 export type Json =
   | string
@@ -78,5 +65,13 @@ export type Database = {
   };
 };
 
-Need to install the following packages:
-  supabase@2.19.7 
+export type Tables = Database['public']['Tables'];
+export type TablesInsert = {
+  [TableName in keyof Tables]: Tables[TableName]['Insert']
+};
+export type TablesUpdate = {
+  [TableName in keyof Tables]: Tables[TableName]['Update']
+};
+export type TablesRow = {
+  [TableName in keyof Tables]: Tables[TableName]['Row']
+}; 
